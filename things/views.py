0df1bbeba388ed75a8826698a641bbf25b3c3433
Oussama Hadder <natsuhadder01@outlook.fr>
2025-05-31 19:34:43 +0200

    fix issues

--- things/views.py
+++ things/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 import rest_framework
+from datetime import timedelta
 
 
 from django.contrib.contenttypes.models import ContentType
